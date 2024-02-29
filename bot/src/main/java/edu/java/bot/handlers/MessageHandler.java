8084737af5c829d4d9b80d7b58780271f1ef14cf
RuslanAndrianov <ruslandomoney@mail.ru>
2024-02-29 17:33:36 +0300

    refactor: small changes

--- bot/src/main/java/edu/java/bot/handlers/MessageHandler.java
+++ bot/src/main/java/edu/java/bot/handlers/MessageHandler.java
@@ -9,9 +9,9 @@ import edu.java.bot.commands.UntrackCommand;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import static edu.java.bot.configuration.CommandsConfig.COMMANDS;
+import static edu.java.bot.repository.in_memory.DBUsersState.getUserState;
 import static edu.java.bot.repository.in_memory.UserState.TRACK;
 import static edu.java.bot.repository.in_memory.UserState.UNTRACKED;
-import static edu.java.bot.repository.in_memory.DBUsersState.getUserState;
 
 @Component
 public class MessageHandler {
