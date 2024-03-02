ab8019cab3bc816fe30777bfacde9c504b175cc7
RuslanAndrianov <ruslandomoney@mail.ru>
2024-03-02 23:36:04 +0300

    fix: trying to fix GitHub build problems (2)

--- shared_dto/src/main/java/edu/java/model/Main.java
+++ shared_dto/src/main/java/edu/java/model/Main.java
@@ -1,5 +1,8 @@
 package edu.java.model;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
 
