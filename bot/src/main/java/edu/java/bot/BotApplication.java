194b7fdd353daad53b40b813cb9bcd2ed1c9c5c5
RuslanAndrianov <ruslandomoney@mail.ru>
2024-04-14 16:25:06 +0300

    fix: added @EnableCaching

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -5,8 +5,10 @@ import edu.java.bot.configs.RetryPolicyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 @EnableConfigurationProperties({ApplicationConfig.class, RetryPolicyConfig.class})
 public class BotApplication {
     public static void main(String[] args) {
